1736a0474bcd81ef2fd8c117d8fb218f01c41b91
coderbekhub <coderbek@mail.ru>
2023-06-20 16:48:15 +0500

    contact tayor

--- src/Components/Smarqand/SamarqandContact.jsx
+++ src/Components/Smarqand/SamarqandContact.jsx
@@ -7,34 +7,33 @@ export default function SamarqandContact() {
   return (
     <section className='samarqandContactSection'>
       <div className="container">
-        <h2>Kontaktlar</h2>
-        <div className='row samarqandContactContent'>
-          <div className='col-md-6 col-12'>
+        <h2 className='contactTitle'>Kontaktlar</h2>
+        <div className='samarqandContactContent'>
+          <div>
             <form>
               <div>
-                <input className='col-12 mb-5' type="text" placeholder='Ism' />
+                <input type="text" placeholder='Ism' />
               </div>
 
               <div>
-                <input className='col-12 mb-5' />
+                <input placeholder='Telefon raqami' />
               </div>
 
               <div>
-                <textarea className='col-12 mb-5 textarea' placeholder='Xabar'></textarea>
+                <textarea placeholder='Xabar'></textarea>
               </div>
 
               <button className='contactBtn'>YUBORISH</button>
             </form>
-
           </div>
-          <div className='samarqandContactInfo col-md-6 col-12'>
+          <div className='samarqandContactInfo'>
             <div>
               <h2>Manzil</h2>
               <p>Toshkent sh., Olmazor ko’chasi, 327А.</p>
             </div>
             <div>
               <h2>Telefon</h2>
-              <a href="tel:+998938092710">+998 (93) 809-27-10</a>
+              <a href="tel:+998938092710">+998 (93) 809-27-10</a> <br />
               <a href="tel:+998331772710">+998 (33) 177-27-10</a>
             </div>
             <div>
