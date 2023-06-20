18bc996ea8dc183f3299d254678570966cafedfd
coderbekhub <coderbek@mail.ru>
2023-06-20 17:21:56 +0500

    formga bot qoshldi

--- src/Components/Smarqand/SamarqandContact.jsx
+++ src/Components/Smarqand/SamarqandContact.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
-// import toast from 'react-hot-toast'
-// import axios from 'axios';
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import axios from 'axios'
 
 export default function SamarqandContact() {
+  const BOT_TOKEN = "5871300926:AAElDbiyIuEm4TFFNo6mzqsgEzJ3kJThimQ";
+  const CHAT_ID = "-1001941898048"
+
+  function formValidationChacking(name, number, message) {
+    let isBoolean = false;
+
+    if (!name) {
+      setError(p => ({ ...p, name: [true, 'F.I.O. kiritilmadi!'] }))
+      isBoolean = true
+
+    } else if (name?.length < 6) {
+      setError(p => ({ ...p, name: [true, "Bunchalik qisqa F.I.O. bo'lmaydi"] }))
+      isBoolean = true
+    }
+
+    if (!number) {
+      setError(p => ({ ...p, phone: [true, 'Telefon raqam kiritilmadi!'] }))
+      isBoolean = true
+    }
+    return isBoolean;
+  }
+
+  const sendForm = (e) => {
+    e.preventDefault()
+    let name = e.target[0].value
+    let number = e.target[1].value
+
+    if (formValidationChacking(name, number)) return
+    setDisbl(true)
+
+    let text = `<b>Ismi:</b> ${name} \n<b>Telefon raqami:</b> ${number} \n`
+
+    axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text
+    }, {
+      headers: {
+        "Content-Type": 'multipart/form-data',
+      }
+    })
+      .then((res) => {
+        toast.success("Habar yuborildi 🙂")
+        e.target.reset()
+        setDisbl(false)
+      })
+      .catch((err) => {
+        toast.error("Habar yuborilmadi! Qaytadan urinib ko'ring 😞")
+        setDisbl(false)
+      })
+
+    let style = {}
+    if (inputValue === '') {
+      style = {
+        border: '2px solid red'
+      }
+    }
+    else {
+      style = {
+        border: '2px solid green'
+      }
+    }
+    setStyled(style)
+  }
+
+  const [styled, setStyled] = useState({ border: '1px solid red', borderRadius: '32px' })
+  const [inputValue, setValue] = useState('')
+  const [disbl, setDisbl] = useState(false)
+  const [error, setError] = useState({
+    name: [false, ""],
+    phone: [false, ""],
+  })
 
   return (
     <section className='samarqandContactSection'>
@@ -10,14 +82,26 @@ export default function SamarqandContact() {
         <h2 className='contactTitle'>Kontaktlar</h2>
         <div className='samarqandContactContent'>
           <div>
-            <form>
-              <div>
-                <input type="text" placeholder='Ism' />
+            <form onSubmit={sendForm}>
+              <div onChange={() => setError(p => ({ ...p, name: [false, ''] }))} >
+                <input type="text" placeholder='Ism' style={error.name[0] ? styled : null}/>
               </div>
+              {error.name[0] && <p style={{ color: 'red' }}>{error.name[1]}</p>}
 
-              <div>
-                <input placeholder='Telefon raqami' />
+              <div onChange={() => setError(p => ({ ...p, name: [false, ''] }))} >
+                <input style={error.name[0] ? styled : null} placeholder='Telefon raqami' autocomplete="off" maxlength="13" minlength="4" type="tel" data-intl-tel-input-id="1"
+                  onChange={(e) => {
+                    e.target.setCustomValidity("");
+                    if (!e.target.validity.valid) {
+                      e.target.setCustomValidity(
+                        "Raqamni noto'g'ri kiritdingiz! Misol: +998977770777"
+                      );
+                    }
+                    setError(p => ({ ...p, phone: [false, ''] }))
+                  }} pattern="[+]{1}[0-9]{3}[0-9]{2}[0-9]{3}[0-9]{2}[0-9]{2}"
+                />
               </div>
+              {error.phone[0] && <p style={{ color: 'red' }}>{error.phone[1]}</p>}
 
               <div>
                 <textarea placeholder='Xabar'></textarea>
@@ -38,7 +122,7 @@ export default function SamarqandContact() {
             </div>
             <div>
               <h2>Pochta</h2>
-              <a href="uztur@gmail.com">uztur@gmail.com</a>
+              <a disabled={disbl} href="uztur@gmail.com">uztur@gmail.com</a>
             </div>
           </div>
         </div>
