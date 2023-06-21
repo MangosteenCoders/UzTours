1d72d7db9e5b1f4800c0f15133a6b895a44ebad7
coderbekhub <coderbek@mail.ru>
2023-06-21 11:30:26 +0500

    ...

---
+++ src/Components/Buxoro/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeHeader from './HomeHeader'
+
+export default function Home() {
+  return (
+    <>
+      <HomeHeader />
+    </>
+  )
+}
---
+++ src/Components/Buxoro/HomeHeader.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../../Images/buxoro.png'
+import img2 from '../../Images/xiva.jpg'
+import img3 from '../../Images/samarkand.jpg'
+
+export default function HomeHeader() {
+  return (
+    <header className='Homehead'>
+      <Carousel className='carusel' fade>
+        <Carousel.Item className='carusel-item'>
+          <img
+            className="d-block w-100"
+            src={img1}
+            alt="First slide"
+          />
+          <Carousel.Caption className='carusel-caption'>
+            <h3>Buxoro</h3>
+            <p>bo'ylab sayohat</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className='carusel-item'>
+          <img
+            className="d-block w-100"
+            src={img2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption className='carusel-caption'>
+            <h3>Xorazim</h3>
+            <p>bo'ylab sayohat</p>
+
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className='carusel-item'>
+          <img
+            className="d-block w-100"
+            src={img3}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption className='carusel-caption'>
+            <h3>Samarqand</h3>
+            <p>bo'ylab sayohat</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </header>
+  )
+}
